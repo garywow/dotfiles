624410a009c0e14f72c206752361e5faf75197dd
tomcode2077 <113079559+tomcode2077@users.noreply.github.com>
2022-10-21 15:53:34 +0800

    Update setup-devenv.sh

--- setup-devenv.sh
+++ setup-devenv.sh
@@ -4,4 +4,6 @@ echo install packages
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y zsh vim dos2unix wget git gh build-essential
+
+cd ~
 wget -q https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh
